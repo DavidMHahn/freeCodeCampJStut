76e854b8c58ff98818d2ff6e54be1edda233abee
DavidMHahn <davidmhahn@gmail.com>
2020-07-24 15:59:05 -0700

    first commit 07/24/2020

--- JavascriptTut1.js
+++ JavascriptTut1.js
@@ -557,3 +557,228 @@ function golfScore(par, strokes) {
 console.log(golfScore(5,4));
 
 //Switch Statements
+/* Write a switch statment which tests val and sets answer for the following conditions:
+1 - "alpha"
+2 - "beta"
+3 - "gamma"
+4 - "delta"
+*/
+function caseInSwitch(val) {
+    var answer = "";
+    switch(val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+    return answer;
+}
+
+console.log(caseInSwitch(1));
+
+//Default Option in Switch Statements
+function switchOfStuff(val) {
+    var answer = "";
+    switch (val) {
+    case "a":
+        answer = "apple";
+        break;
+    case "b":
+        answer = "bird";
+        break;
+    case "c":
+        answer = "cat";
+         break;
+    default:  //provides a default output when one of the cases is not met.
+        answer = "stuff";
+        break;
+}
+return answer;
+}
+console.log(switchOfStuff("a"));
+
+//Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+    var answer = "";
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+    }
+    return answer;
+}
+
+console.log(sequentialSizes(1));
+
+//Replacing If Else Chains with Switch statement
+function chainToSwitch(val) {
+    var answer = "";
+    switch(val) {
+        case "Bob":
+            answer = "Marley";
+            break;
+        case "42":
+            answer = "The Answer";
+            break;
+        case "1":
+            answer = "There is no #1";
+            break;
+        case "99":
+            answer = "Missed me by this much!";
+            break;
+        case "7":
+            answer = "Ate Nine";
+            break;   
+    }
+/*  if (val === "Bob") {  ***The commented out code is what the switch statement replaces***
+        answer = "Marley";
+    }
+    else if (val === 42) {
+        answer = "The Answer";
+    }
+    else if (val === 1) {
+        answer = "There is no #1";
+    }
+    else if (val === 99) {
+        answer = "Missed me by this much!";
+    }
+    else if (val === 7) {
+        answer = "Ate Nine";
+    }
+*/
+    return answer;
+}
+
+console.log(chainToSwitch());
+
+//Returning Booolean Values from Functions
+function isLess(a, b) {
+    return a < b;
+
+/*    if (a < b)  ***The commented out code is replaced with a simple return statement.    Recall that all comparison operators return a boolean (True/False) value.***
+        return true;
+    }
+    else {
+        return false;
+    }
+*/
+}
+console.log(isLess(10, 15));
+
+//Returning Early Pattern from Function
+function abTest(a, b) {
+
+    if (a < 0 || b < 0) {
+        return undefined;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+console.log(abTest(2,2)); // square root of 2 + square root of 2 = 2.828, 2.828 squared = 7.99, rounded up equals 8.
+
+//Counting Cards
+var count = 0; //global count variable
+
+function cc(card) {
+    switch(card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        //no need to add or subtract for 7, 8, or 9.
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count --;
+            break;
+    }
+    var holbet = "Hold";
+    if (count > 0) {
+        holdbet = "Bet";
+    }
+    return count + " " + holdbet;
+}
+
+cc(2); cc(3); cc(7); cc("K"); cc("A");
+console.log(cc(4));
+
+//Build Javascript Objects
+var ourDog = {      //Generic Dog object
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+var myDog = {       //Our Dog Object based on Generic Dog object
+    "name": "Quincy",
+    "legs": 3,
+    "tails": 2,
+    "friends": []
+};
+
+//Accessing Object Properties with Dot Notation
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+var hatValue = testObj.hat; //assigns the value of the hat item in the object to hatValue
+var shirtValue = testObj.shirt; //assigns the value of the shirt item to shirtValue
+
+console.log(hatValue); //prints ballcap
+console.log(shirtValue);//prints jersey
+
+//Accessing Object Properties with Bracket Notation
+var testObj = {
+    "an entree": "hamburger", //must use bracket notation if there is a space in the property value
+    "my side": "veggies",
+    "the drink": "water",
+};
+
+var entreeValue = testObj["an entree"]; //note that you will use either ""
+var drinkValue = testObj['the drink'];  //or '' for strings inside the brackets.
+
+console.log(entreeValue);
+console.log(drinkValue);
+
+//Accessing Object Properties with Variables
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+var playerNumber = 16; //create a variable and assign value of 16 which corresponds to a player name in the testObj.
+var player = testObj[playerNumber]; //pass the variable playerNumber (16) into the testObj and assign that value to player. Since 16 is "Montana", the testObj will return "Montana" and assign it to the variable player.
+var player2 = testObj[19]; //More directly, this assigns "Unitas" to the variable player2, without using an additional variable.
+
+console.log(player); //prints out Montana
+console.log(player2); //prints out Unitas
+

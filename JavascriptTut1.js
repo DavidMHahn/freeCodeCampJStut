776c77b72e3abe8f502d02b31e409c5ece585b4c
DavidMHahn <davidmhahn@gmail.com>
2020-07-25 16:14:41 -0700

    Second commit 07/25/2020

--- JavascriptTut1.js
+++ JavascriptTut1.js
@@ -963,3 +963,45 @@ var secondTree = myPlants[1].list[1]; //remember that arrays start counting at 0
 console.log(secondTree);
 
 //Record Collection
+var collection = {
+    "2548": {
+        "album": "Slippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    "2468": {
+        "album": "1999",
+        "artist": "Prince",
+        "tracks": [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": [ ]
+    },
+    "5439": {
+        "album": "ABBA Gold"
+    }
+};
+var collectionCopy = JSON.parse(JSON.stringify(collection)); //Keep a copy of the collection for tests. This makes a copy of the collection object.
+
+function updateRecords(id, prop, value) {
+    if (value === "") {
+        delete collection[id][prop]
+    }
+    else if (prop === "tracks") {
+        collection[id][prop] = collection[id][prop] || [];
+        collection[id][prop].push(value);
+    }
+    else {
+        collection[id][prop] = value;
+    }
+
+    return collection;
+}
+console.log(updateRecords(5439, "artist", "ABBA"));

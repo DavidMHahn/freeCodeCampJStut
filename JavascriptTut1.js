e317d92d7f22a6437a1dfb0ba7924127897cadd1
DavidMHahn <davidmhahn@gmail.com>
2020-07-28 14:12:46 -0700

    Commit 07/28/2020

--- JavascriptTut1.js
+++ JavascriptTut1.js
@@ -1455,13 +1455,165 @@ const increment = (function() {
 console.log(increments(5, 2));
 console.log(increments(5));
 
-//Use the Rest Operator (...) with Function Parameters. The rest operator allows a variable number of arguments.
+//Use the Rest Operator (...) with Function Parameters. The rest operator allows you to create a function with a variable number of arguments.
 const sum = (function() {
     return function sum(x, y, z) {
         const args = [ x, y, z ];
         return args.reduce((a, b) => a + b, 0);
     };
 })();
-console.log(sum(1, 2, 3));
+console.log(sum(1, 2, 3)); //Prints 6.
 
 //Converting the code above into using the Rest Operator
+const sum = (function() {
+    return function sum(...args) { //... allows for any number of arguments to be passed in.
+        //const args = [ x, y, z ]; commented out
+        return args.reduce((a, b) => a + b, 0);
+    };
+})();
+console.log(sum(1, 2, 3, 4)); //Prints 10
+
+//Use the Spread Operator to Evaluate Arrays In-place. Spread operator looks like the rest operator (...), but it expands/spreads an already existing array into the individual parts. Can only be used in an argument to a function or in an array literal.
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+    arr2 = arr1;
+    arr1[0] = 'potato'
+})();
+console.log(arr2); //prints ["potato", "FEB", "MAR", "APR", "MAY"] in the above example the array is changed to replace the 0th element from 'JAN' to 'potato'
+//Rewriting the above code to use the Spread Operator
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+    arr2 = [...arr1]; //using the spread operator inside brackets (array) to make arr2 equal to the contents of array 1.
+    arr1[0] = 'potato'
+})();
+console.log(arr2); //prints ["JAN", "FEB", "MAR", "APR", "MAY"]
+console.log(arr1); //prints ["potato", "FEB", "MAR", "APR", "MAY"]
+
+//Use Destructuring Assignment to Assign Variables from Objects
+var voxel = {x: 3.6, y: 7.4, z: 6.54}; //object with 3 elements
+var x = voxel.x; // x = 3.6     This is the old way of assigning each element of the
+var y = voxel.y; // y = 7.4     object with it's own variable.
+var z = voxel.z; // z = 6.54
+
+const { x : a, y : b, z : c} = voxel;
+
+const AVG_TEMPERATURES = {
+    today: 77.5,
+    tomorrow: 79
+};
+function getTempOfTmrw(avgTemperatures) {
+    "use strict";
+    const tempOfTomorrow = undefined;
+}
+
+//Rewriting the above code using Destructuring Assignment
+
+const AVG_TEMPERATURES = {
+    today: 77.5,
+    tomorrow: 79
+};
+function getTempOfTmrw(avgTemperatures) {
+    "use strict";
+    const { tomorrow : tempOfTomorrow } = avgTemperatures; //Destructuring syntax
+    return tempOfTomorrow;
+}
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); //Prints 79
+
+//Why not this way?
+const AVG_TEMPERATURES = {
+    today: 77.5,
+    tomorrow: 79
+};
+function getTempOfTmrw() {
+    "use strict";
+    const { tomorrow : tempOfTomorrow } = AVG_TEMPERATURES; //Destructuring syntax
+    return tempOfTomorrow;
+}
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); //Also prints 79
+
+//This works too
+const AVG_TEMPERATURES = {
+    today: 77.5,
+    tomorrow: 79
+};
+function getTempOfTmrw(AVG_TEMPERATURES) {
+    "use strict";
+    const { tomorrow : tempOfTomorrow } = AVG_TEMPERATURES; //Destructuring syntax
+    return tempOfTomorrow;
+}
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // Prints 79
+
+//Destructuring Assignment with Nested Objects
+const LOCAL_FORCAST = {                 //Nested objects
+    today: { min: 72, max: 83 },
+    tomorrow: { min: 73.3, max: 84.6}
+};
+function getMaxOfTmrw(forecast) {
+    "use strict";
+
+    const { tomorrow : { max : maxOfTomorrow }} = forecast; //Gets max from tomorrow and sets it to maxOfTomorrow.
+
+    return maxOfTomorrow;
+}
+console.log(getMaxOfTmrw(LOCAL_FORCAST)); //Prints 84.6
+
+//Use Destructuring Assignment to Assign Variables from Arrays
+const [z, x] = [1, 2, 3, 4, 5, 6]; //assigns z and x to the first 2 variables of the array by default
+const [z, x, , y] = [1, 2, 3, 4, 5, 6]; //assigns z to first element, x to second element, skips third element, and assigns y to fourth element.
+console.log(z, x, y); //prints 1, 2, 4
+
+//Destructuring to switch places of a and b
+let a = 8, b = 6;
+(() => {
+    "use strict";
+    [a, b] = [b, a]
+})();
+console.log(a);
+console.log(b);
+
+//Use destructuring Assignment with the Rest Operator
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+    const [ , , ...arr] = list; //removes first two variables by assigning them no value
+    return arr;
+}
+const arr = removeFirstTwo(source)
+console.log(arr); //Prints [3,4,5,6,7,8,9,10]
+
+//Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34;
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+const half = (function() {
+    return function half(stats) { //passing in the entire stats object as function argument
+        return (stats.max + stats.min) / 2.0 //but we only need max and min
+    };
+})();
+console.log(stats);
+console.log(half(stats));
+
+//Rewriting the above code to pass in only what we need. This is commonly used with API calls/AJAX requests
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34;
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+const half = (function() {
+    return function half({ max, min }) { //passing in only what we need
+        return (max + min) / 2.0
+    };
+})();
+console.log(stats);
+console.log(half(stats));
+
+//Create Strings using Template Literals
